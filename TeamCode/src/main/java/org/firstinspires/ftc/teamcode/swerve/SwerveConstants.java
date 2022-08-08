766ef86452edc6e96302b9b6b1d28a842b1d096e
Hunter Tsai <awesomehtr1@gmail.com>
2022-08-07 20:27:22 -0700

    swerve go brrrr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/SwerveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/SwerveConstants.java
@@ -2,6 +2,6 @@ package org.firstinspires.ftc.teamcode.swerve;
 
 public class SwerveConstants {
     public static final double radius = 13.2283464567; //336mm trackwidth
-    public static final double trackWidth = 13.2283464567;//radius * Math.cos(Math.toRadians(45)); //9.35385501181
+    public static final double trackWidth = 13.2283464567; //radius * Math.cos(Math.toRadians(45)); //9.35385501181
     public static final double wheelBase = radius * Math.sin(Math.toRadians(45)); //9.35385195502
 }
